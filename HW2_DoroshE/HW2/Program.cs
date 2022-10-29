df1f5c26faa36d3ced5cd643503cacbe2dda6f84
nonseverus <evdorow@gmail.com>
2022-10-29 12:48:24 +0300

    task 2

--- HW2_DoroshE/HW2/Program.cs
+++ HW2_DoroshE/HW2/Program.cs
@@ -1,6 +1,6 @@
 ﻿using HW2;
 
-Task1 matrix = new Task1();
+/*Task1 matrix = new Task1();*/
 
 /*int[,] snakeMatrix1 = matrix.VerticalSnake(3, 4);
 matrix.Print(snakeMatrix1);*/
@@ -15,6 +15,17 @@ matrix.Print(snakeMatrix3);
 /*int[,] snakeMatrix3 = matrix.VerticalSpiralSnake(4, 6);
 matrix.Print(snakeMatrix3);*/
 
-int[] pixMatrix = { 0, 1, 2, 3, 3, 3 };
+/*int[] pixMatrix = { 0, 1, 3, 3, 4, 4 };
 int[] pixMatrix1 = { 1, 1, 1, 2};
-int[] pixMatrix2 = { 1, 1, 1, 1};
+int[] pixMatrix2 = { 1, 2, 3, 4};*/
+
+/*Task2 matrix = new Task2();*/
+
+/*var mat = matrix.LongestHorizontalLine(pixMatrix);
+var mat1 = matrix.LongestHorizontalLine(pixMatrix1);
+var mat2 = matrix.LongestHorizontalLine(pixMatrix2);*/
+
+/*matrix.Print(mat);
+matrix.Print(mat1);
+matrix.Print(mat2);
+*/
--- HW2_DoroshE/HW2/Task2.cs
+++ HW2_DoroshE/HW2/Task2.cs
@@ -8,20 +8,63 @@ namespace HW2
 {
     internal class Task2
     {
-        public int LongestLine(int[] mat)
+        public List<KeyValuePair<int, int[]>> LongestHorizontalLine(int[] mat)
         {
-            Dictionary<int, int[]> res = new Dictionary<int, int[]>();
+            Dictionary<int, int[]> dict = new Dictionary<int, int[]>();
 
-            int color;
-            int start;
-            int end;
+            int color = mat[0];
+            int start = 0;
 
             for (int i = 0; i < mat.Length; i++)
             {
-                color = 0;
+                if (color != mat[i])
+                {
+                    if (dict.ContainsKey(color))
+                    {
+                        if (dict[color][1] - dict[color][0] < (i - 1) - start)
+                        {
+                            dict[color][0] = start;
+                            dict[color][1] = i - 1;
+                            dict[color][2] = i - start;
+                        }
+                        color = mat[i];
+                        start = i;
+                        dict.Add(color, new int[] { start, i, 1 });
+                    }
+                    else
+                    {
+                        dict.Add(color, new int[] { start, i - 1, 1});
+                        color = mat[i];
+                        start = i;
+                    }
+                }
+                else
+                {
+                    if (dict.ContainsKey(color))
+                    {
+                        ++dict[color][1];
+                        ++dict[color][2];
+                    }
+                    else
+                    {
+                        dict.Add(color, new int[] { start, i, i - start + 1});
+                    }
+                }
             }
 
-            return 0;
+            int maxLength = dict.Max(x => (x.Value[1] - x.Value[0]));
+
+            var list = dict.Where(x => (x.Value[1] - x.Value[0]) == maxLength).ToList();
+
+            return list;
+        }
+
+        public void Print(List<KeyValuePair<int, int[]>> dict)
+        {
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"Color: {item.Key}; Start index: {item.Value[0]}, End inedx: {item.Value[1]}; Length: {item.Value[2]};");
+            }
         }
     }
 }
